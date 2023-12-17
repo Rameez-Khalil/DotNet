0a7a4cbd8707ed3c3d76ca2d1255cf7d4c93fd8b
rameez.khalil <rameezkhalil23@gmail.com>
2023-12-17 08:45:30 -0800

    exception-handling

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+
+    public class Program
+        
+    {
+        //No need to create object.
+        static void Main(string[] args)
+        {
+
+            int number1 = 5;
+            int number2 = 0;
+            try
+            {
+                Console.WriteLine("Calling Do Division: ");
+                doDivision(number1, number2);
+            }
+            catch (DivideByZeroException e)
+            {
+                //Console.WriteLine("You cannot divide by Zero");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+        static void doDivision(int number1, int number2)
+        {
+            if (number2 == 0)
+            {
+                throw new DivideByZeroException(); 
+            }
+            Console.WriteLine(number2/number1);
+        }
+
+    }
+}
