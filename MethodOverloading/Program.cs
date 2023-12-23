b15d7606dfeca69d7f1799c7eaf917da3d5939a1
rameez <rameezkhalil23@gmail.com>
2023-12-23 14:28:40 -0800

    methodoverloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+namespace App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HelloWorld("rameez");
+            HelloWorld("rameez", 2); 
+
+        }
+
+
+        static void HelloWorld(string s, int id) {
+            Console.WriteLine(s + id);
+        }
+        static void HelloWorld(string s) { 
+            Console.WriteLine(s);
+        }
+    }
+}
