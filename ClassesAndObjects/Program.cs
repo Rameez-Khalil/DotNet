a2f95ac4b4c86ffc3e20555c3b8549a5e7f990ab
rameez <rameezkhalil23@gmail.com>
2023-12-23 14:49:36 -0800

    classes and objects

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,49 @@
+﻿using System; 
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog1 = new Animal("Edgar", "Warf");
+            Animal dog2 = new Animal("Edgar", "Warf");
+            
+            Console.WriteLine(Animal.AnimalCount());  
+
+
+        }
+    }
+
+    class Animal
+    {
+        //fields are methods and names.
+        public String name;
+        public String sound;
+        static int count = 0;
+
+       public Animal(String name, String sound)
+        {
+            this.name = name;
+            this.sound = sound;
+            count++;
+        }
+
+        Animal()
+        {
+            name = "no name";
+            sound = "no sound"; 
+            count++;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(this.name + " " + this.sound);
+        }
+
+        public static int AnimalCount()
+        {
+            return count; 
+        }
+
+    }
+}
---
+++ EnumartedTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace EnumeratedTypes
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            CarColor carColor = CarColor.Green;
+            PaintCar(carColor);
+        
+        }
+
+        enum CarColor: byte
+        {
+            Orange = 1, 
+            Blue, 
+            Green, 
+            Red, 
+            Yellow
+        }
+
+        static void PaintCar(CarColor color)
+        {
+            Console.WriteLine("Car has been painted with: " + (int) color);
+        }
+    }
+}
