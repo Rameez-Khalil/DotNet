885d5c3ace147bbaec22320a377b4ac0b18f5111
rameez <rameezkhalil23@gmail.com>
2023-12-23 12:40:44 -0800

    functions

---
+++ Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int number1 = 20; 
+            int number2 = 30;
+            int result = AddNumbers(number1, number2);
+            Console.WriteLine(result);
+
+
+        }
+
+        //private static void helloWorld(String name)
+        //{
+        //    Console.WriteLine("Hello World: " + name); 
+        //}
+
+        private static int AddNumbers(int number, int number2) {
+               return number + number2;
+        }
+    }
+}
