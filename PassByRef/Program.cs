865ab61ce1383e87e12063ac923e4494a848619d
rameez <rameezkhalil23@gmail.com>
2023-12-23 12:48:50 -0800

    passbyref

--- PassByRef/Program.cs
+++ PassByRef/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+namespace PassByRef
+{
+
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            int number1 = 10; 
+            int number2 = 20;
+            SwapNumbers(ref number1, ref number2);
+
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+
+            }
+
+
+        static void SwapNumbers(ref int number1, ref int number2)
+            {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            }
+        }
+}
--- PassByRef/obj/Debug/net8.0/PassByRef.AssemblyInfo.cs
+++ PassByRef/obj/Debug/net8.0/PassByRef.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PassByRef")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885d5c3ace147bbaec22320a377b4ac0b18f5111")]
 [assembly: System.Reflection.AssemblyProductAttribute("PassByRef")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PassByRef")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
