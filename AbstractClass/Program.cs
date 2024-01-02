e0e323b97a3b43d48d7889c3656256ebdc7e018f
rameez.khalil <rameezkhalil23@gmail.com>
2024-01-02 02:16:39 -0800

    abstract classes

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AbstractClass
+{
+    class Program
+    {
+        public static void Main(string[] args) { 
+            Triangle triangle = new Triangle();
+            triangle.name = "Triangle";
+            triangle.GetInformation();
+            double results = triangle.GetArea(10.00,10.00);
+            Console.WriteLine(results);
+
+        }
+
+    }
+}
---
+++ AbstractClass/Shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+
+    //This class can never be instantiated.
+    //This is just a roadmap for a class.
+    /*
+     * Non abstract methods are applicable.
+     * Concrete methods are also allowed.
+     */
+    abstract class Shape
+    {
+        public String name { get; set; }
+       
+        //This is an informative method for this class.
+        public void GetInformation()
+        {
+            Console.WriteLine(name + " This is the only information");
+        }
+
+        public abstract double GetArea(double width, double height); 
+        
+
+
+    }
+
+    class Triangle : Shape
+    {
+
+        public override double GetArea(double width, double height)
+        {
+            return (width*height)/2;
+        }
+
+    }
+}
